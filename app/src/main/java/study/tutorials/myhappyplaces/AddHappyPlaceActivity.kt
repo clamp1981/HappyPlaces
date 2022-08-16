cab3a2904e1b204fd17a4cc348abe6845417a6ff
clamp1981 <clamp1981@gmail.com>
2022-08-16 22:57:12 +0900

    feat : add view binding ( button and toolbar )

--- app/src/main/java/study/tutorials/myhappyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/study/tutorials/myhappyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,23 @@ package study.tutorials.myhappyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import study.tutorials.myhappyplaces.databinding.ActivityAddHappyPlaceBinding
+import study.tutorials.myhappyplaces.databinding.ActivityMainBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+    var binding : ActivityAddHappyPlaceBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        //setContentView(R.layout.activity_add_happy_place)
+        setSupportActionBar(binding?.toolbarAddPlace)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        //뒤로가기 버튼
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
     }
 }
--- app/src/main/java/study/tutorials/myhappyplaces/MainActivity.kt
+++ app/src/main/java/study/tutorials/myhappyplaces/MainActivity.kt
@@ -1,11 +1,22 @@
 package study.tutorials.myhappyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+import study.tutorials.myhappyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var binding : ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        binding?.fabAddHappyPlace?.setOnClickListener {
+            val intent = Intent( this, AddHappyPlaceActivity::class.java )
+            startActivity(intent)
+        }
     }
 }
